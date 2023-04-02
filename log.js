f0f3a367e759f0dd7cc9bfeaa13c5737b2adacd0
haloged <aokhfj@outlook.com>
2023-04-02 14:07:09 +0800

    更改log

--- log.js
+++ log.js
@@ -1,6 +1,6 @@
 //控制台输出
 
-var version = 'v0.0.1(0001)-dev' //版本号
+var version = 'v0.1.6(0001)-dev' //版本号
 console.log(`%c
 H                  H
 H                  H
@@ -13,7 +13,7 @@ H                  H
 H                  H
 H                  H
 `, "font-weight:bold;color:#6d50f0")
-console.log('欢迎访问haloged 的博客！')
+console.log('欢迎访问haloged studio 官方网站！')
 console.log('感谢haloged studio的所有开发者们!');
 console.log('假装是一个小小的彩蛋~');
 console.log("" + version + " ©️ 2022 haloged studio, All rights reserved.")
