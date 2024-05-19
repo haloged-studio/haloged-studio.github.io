d7202648df47bc424d4c31cd86c3ecf2d63426e9
haloged <101408211+haloged@users.noreply.github.com>
2024-05-19 11:47:31 +0800

    [feet]更改彩蛋内容！

--- log.js
+++ log.js
@@ -1,6 +1,6 @@
 //控制台输出
 
-var version = 'v0.3.8(0038)-dev' //版本号
+var version = 'v0.3.9(0045)-dev' //版本号
 console.log(`
    __ __     __                 __    ______          ___    
   / // /__ _/ /__  ___ ____ ___/ /   / __/ /___ _____/ (_)__ 
@@ -10,5 +10,6 @@ console.log(`
 `)
 console.log('欢迎访问haloged studio 官方网站！')
 console.log('感谢haloged studio的所有开发者们!');
+console.log('想加入Haloged Studio吗？加入团队请发送邮件至hr@haloged-studio.eu.org(备注来自console~)')
 console.log('假装是一个小小的彩蛋~');
-console.log("" + version + " ©️ 2022-2023 haloged studio, All rights reserved.")
+console.log("" + version + " ©️ 2022-2024 haloged studio, All rights reserved.")
