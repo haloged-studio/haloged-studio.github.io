f59362278946020ec26dce5aa446b181f2d8be9d
haloged <101408211+haloged@users.noreply.github.com>
2024-05-19 12:04:19 +0800

    [fix]更改console不能显的Bug

--- log.js
+++ log.js
@@ -1,13 +1,7 @@
 //控制台输出
 
 var version = 'v0.3.9(0045)-dev' //版本号
-console.log(`
-   __ __     __                 __    ______          ___    
-  / // /__ _/ /__  ___ ____ ___/ /   / __/ /___ _____/ (_)__ 
- / _  / _ `/ / _ \/ _ `/ -_| _  /   _\ \/ __/ // / _  / / _ \
-/_//_/\_,_/_/\___/\_, /\__/\_,_/   /___/\__/\_,_/\_,_/_/\___/
-                 /___/                                       
-`)
+console.log('%cHaloged Studio', ' text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25);font-size:5em');
 console.log('欢迎访问haloged studio 官方网站！')
 console.log('感谢haloged studio的所有开发者们!');
 console.log('想加入Haloged Studio吗？加入团队请发送邮件至hr@haloged-studio.eu.org(备注来自console~)')
